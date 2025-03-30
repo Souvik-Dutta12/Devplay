f63c1695a3ed08c3c632a48d59cfa5d5c655a782
Souvik Dutta <souvikdutta5122005@gmail.com>
2025-03-30 12:19:40 +0530

    ✅

--- backend/src/controllers/dashboard.controller.js
+++ backend/src/controllers/dashboard.controller.js
@@ -7,12 +7,26 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-})
+   
+});
+
+
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-    // TODO: Get all the videos uploaded by the channel
-})
+    const { channelId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(channelId)) {
+        throw new ApiError(400, "Invalid channel ID");
+    }
+
+    const videos = await Video.find({ channelId });
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, videos, "Channel videos retrieved successfully")
+    );
+});
 
 export {
     getChannelStats, 
